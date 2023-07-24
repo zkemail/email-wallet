eca34fe59821f4891111e9b9905c3c6b4c2a7cf6
Saleel <saleel@saleel.xyz>
2023-07-24 23:01:52 +0530

    chore: update proofgen.sh to work with both docker/local RS

--- packages/relayer/proofgen.sh
+++ packages/relayer/proofgen.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -e # Stop on error
+
+# Ensure nonce is set
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <nonce>"
     exit 1
@@ -5,59 +10,48 @@ fi
 
 # Read nonce from args
 NONCE=$1
-
 CIRCUIT_NAME="wallet"
 
 # Set file paths needed
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$(dirname $(dirname "$SCRIPT_DIR"))
 CIRCUITS_PATH="${PROJECT_ROOT}/packages/circuits"
 CIRCUITS_BUILD_PATH="${PROJECT_ROOT}/packages/circuits/build"
 RELAYER_PATH="${PROJECT_ROOT}/packages/relayer"
-PROOF_DIR_PATH="${RELAYER_PATH}/proofs"
-
+PROOF_DIR_PATH="${RELAYER_PATH}/proofs/"
 EML_PATH="${RELAYER_PATH}/received_eml/wallet_${NONCE}.eml"
 INPUT_JSON_PATH="${PROOF_DIR_PATH}/input_${NONCE}.json"
 WITNESS_PATH="${PROOF_DIR_PATH}/witness_${NONCE}.wtns"
+ZKEY_PATH="${CIRCUITS_BUILD_PATH}/${CIRCUIT_NAME}.zkey"
 
-RAPIDSNARK_PROVER_PATH="${PROJECT_ROOT}/lib/rapidsnark/build/prover"
+# ⏵⏵⏵⏵⏵⏵ NOTE: Comment/Uncomment below lines to use local or docker rapidsnark 
+# RAPIDSNARK_PATH="${PROJECT_ROOT}/lib/rapidsnark/build/prover"
+RAPIDSNARK_PATH="docker run --rm -i -t -v ${PROOF_DIR_PATH}:${PROOF_DIR_PATH} -v ${CIRCUITS_BUILD_PATH}:/${CIRCUITS_BUILD_PATH} rapidsnark"
 
-echo "\n"
 
 # Make sure inputs dir exists
-mkdir -p "${PROOF_DIR_PATH}" 
+mkdir -p "${PROOF_DIR_PATH}"
 echo "✓ Inputs dir: ${PROOF_DIR_PATH}"
 
 # Generate input
-npx tsx "${CIRCUITS_PATH}/scripts/generate-input.ts" --email-file="${EML_PATH}" --nonce="${NONCE}" --output="${INPUT_JSON_PATH}" --silent | tee /dev/stderr
-status_inputgen=$?
-if [ $status_inputgen -ne 0 ]; then
-    echo "✘ Input generation failed: ${status_inputgen}"
-    exit 1
-fi
+npx tsx "${CIRCUITS_PATH}/scripts/generate-input.ts" --email-file="${EML_PATH}" --nonce="${NONCE}" --output="${INPUT_JSON_PATH}" --silent
 echo "✓ Input generated successfuly"
 
-
 # Generate witness
-node "${CIRCUITS_BUILD_PATH}/${CIRCUIT_NAME}_js/generate_witness.js" "${CIRCUITS_BUILD_PATH}/${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm" "${INPUT_JSON_PATH}" "${WITNESS_PATH}" | tee /dev/stderr
-status_jswitgen=$?
-if [ $status_jswitgen -ne 0 ]; then
-    echo "✘ Witness generation failed: ${status_jswitgen}"
-    exit 1
-fi
+node "${CIRCUITS_BUILD_PATH}/${CIRCUIT_NAME}_js/generate_witness.js" "${CIRCUITS_BUILD_PATH}/${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm" "${INPUT_JSON_PATH}" "${WITNESS_PATH}"
 echo "✓ Witness generated successfuly"
 
+# Generate proof using Rapidsnark - This works for both docker and local rapidsnark
+${RAPIDSNARK_PATH} \
+        ${ZKEY_PATH} \
+        ${PROOF_DIR_PATH}/witness_${NONCE}.wtns \
+        ${PROOF_DIR_PATH}/rapidsnark_proof_${NONCE}.json \
+        ${PROOF_DIR_PATH}/rapidsnark_public_${NONCE}.json 
+echo "✓ Proof generated successfuly"
 
-# Generate proof using Rapidsnark
-docker run \
-    --rm \
-    -it \
-    -v ${RELAYER_PATH}/proofs:/tmp \
-    -v ${CIRCUITS_BUILD_PATH}:/circuit \
-    rapidsnark \
-        /circuit/${CIRCUIT_NAME}.zkey \
-        /tmp/witness_${NONCE}.wtns \
-        /tmp/proof_${NONCE}.json \
-        /tmp/public_${NONCE}.json
+# Call relayer to send proof to chian
+# TODO: Upgrade debug -> release and edit dockerfile to use release
+"${PROJECT_ROOT}/target/debug/relayer" chain false "${PROOF_DIR_PATH}" "${NONCE}"
+echo "✓ Sent to chain successfuly"
 
-# TODO: Call relayer with proof
+exit 0
