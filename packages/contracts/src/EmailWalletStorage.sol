3f8a59fb4b230aa6218f9f6638e6b1c0529cd52d
Saleel <saleel@saleel.xyz>
2023-07-28 00:49:24 +0530

    chore: remove existing contracts and add hardhat

--- packages/contracts/src/EmailWalletStorage.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract EmailWalletStorage {
-    mapping(uint256 => bool) public nullifier;
-    mapping(bytes32 => address) public wallets;
-}
