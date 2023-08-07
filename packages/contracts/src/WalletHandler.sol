a71171564bdd0ae72f1aa41a2a02ccdb70602bae
Saleel <saleel@saleel.xyz>
2023-08-07 23:58:30 +0530

    feat(contracts): implement ERC20 wallet trasnfer

--- packages/contracts/src/WalletHandler.sol
+++ packages/contracts/src/WalletHandler.sol
@@ -10,7 +10,8 @@ import "./interfaces/Constants.sol";
 
 contract WalletHandler is TokenRegistry {
     function getAddressOfSalt(bytes32 salt) public view returns (address) {
-        return Create2.computeAddress(salt, keccak256(type(Wallet).creationCode));
+        return
+            Create2.computeAddress(salt, keccak256(type(Wallet).creationCode));
     }
 
     function _processETHTransferRequest(
@@ -38,12 +39,16 @@ contract WalletHandler is TokenRegistry {
         address tokenAddress = getTokenAddress(tokenName);
         require(tokenAddress != address(0), "unsupported token");
 
-        IERC20 token = IERC20(tokenAddress);
+        Wallet sender = Wallet(payable(getAddressOfSalt(senderSalt)));
 
-        token.transferFrom(
-            getAddressOfSalt(senderSalt),
-            getAddressOfSalt(recipientSalt),
-            amount
+        sender.execute(
+            tokenAddress,
+            0,
+            abi.encodeWithSignature(
+                "transfer(address,uint256)",
+                getAddressOfSalt(recipientSalt),
+                amount
+            )
         );
     }
 
@@ -56,7 +61,12 @@ contract WalletHandler is TokenRegistry {
         if (Strings.equal(tokenName, Constants.ETH_TOKEN_NAME)) {
             _processETHTransferRequest(senderSalt, recipientSalt, amount);
         } else {
-            _processERC20TransferRequest(senderSalt, recipientSalt, tokenName, amount);
+            _processERC20TransferRequest(
+                senderSalt,
+                recipientSalt,
+                tokenName,
+                amount
+            );
         }
     }
 }
--- packages/contracts/test/wallet.unit.test.ts
+++ packages/contracts/test/wallet.unit.test.ts
@@ -12,7 +12,7 @@ import {
   stripZerosLeft,
 } from "ethers";
 import { expect } from "chai";
-import { EmailWalletCore } from "../typechain-types";
+import { EmailWalletCore, TestERC20 } from "../typechain-types";
 
 const mockProof = new Uint8Array([1]);
 
@@ -21,9 +21,18 @@ const mockProof = new Uint8Array([1]);
  */
 describe("Email Wallet Contracts > Wallet", function () {
   let coreContract: EmailWalletCore;
+  let erc20Contract: TestERC20;
   let owner: Signer;
   let relayer: Signer;
   const domainPublicKeyHash = keccak256(new Uint8Array([4, 3, 6]));
+
+  const senderPointer = encodeBytes32String("SP");
+  const senderIndicator = encodeBytes32String("SI");
+  const recipientPointer = encodeBytes32String("RP");
+  const recipientIndicator = encodeBytes32String("RI");
+
+  const senderWalletSalt = encodeBytes32String("SND");
+  const recipientWalletSalt = encodeBytes32String("RCP");
 
   before(async function () {
     [owner, relayer] = await ethers.getSigners();
@@ -35,6 +44,14 @@ describe("Email Wallet Contracts > Wallet", function () {
       owner
     )) as EmailWalletCore;
 
+    erc20Contract = (await ethers.deployContract(
+      "TestERC20",
+      [parseEther("10")],
+      owner
+    )) as TestERC20;
+
+    await coreContract.setTokenAddress("TST", erc20Contract.getAddress());
+
     // Register main relayer used in all other tests
     await coreContract.connect(relayer).registerRelayer(id("R1"));
 
@@ -42,33 +59,33 @@ describe("Email Wallet Contracts > Wallet", function () {
       "ethereum.org",
       domainPublicKeyHash
     );
-  });
 
-  it.only("should be able to send ETH to another email", async function () {
-    // Create sender and initialize it
-    const senderPointer = encodeBytes32String("SP");
-    const senderIndicator = encodeBytes32String("SI");
-    const recipientPointer = encodeBytes32String("RP");
-    const recipientIndicator = encodeBytes32String("RI");
-
-    const senderWalletSalt = encodeBytes32String("SND");
-    const recipientWalletSalt = encodeBytes32String("RCP");
-
+    // Create Sender
     await coreContract
       .connect(relayer)
       .createAccount(senderPointer, senderIndicator, mockProof);
     await coreContract
       .connect(relayer)
       .initializeAccount(senderPointer, senderIndicator, mockProof);
+
+    // Create sender wallet
     await coreContract
       .connect(relayer)
       .createWallet(senderWalletSalt, 123, senderIndicator, mockProof);
+
+    // Register recipient
+    await coreContract
+      .connect(relayer)
+      .createAccount(recipientPointer, recipientIndicator, mockProof);
+
+    // Create recipient wallet
     await coreContract
       .connect(relayer)
       .createWallet(recipientWalletSalt, 123, senderIndicator, mockProof);
+  });
 
+  it("should be able to send ETH to another email", async function () {
     const senderAddress = await coreContract.getAddressOfSalt(senderWalletSalt);
-    console.log(senderAddress);
 
     // Transfer some ETH to the sender
     await owner.sendTransaction({
@@ -79,11 +96,6 @@ describe("Email Wallet Contracts > Wallet", function () {
     expect(await ethers.provider.getBalance(senderAddress)).to.equal(
       parseEther("5")
     );
-
-    // Register recipient
-    await coreContract
-      .connect(relayer)
-      .createAccount(recipientPointer, recipientIndicator, mockProof);
 
     await coreContract.connect(relayer).executeEmailOp({
       senderPointer,
@@ -118,6 +130,52 @@ describe("Email Wallet Contracts > Wallet", function () {
     });
 
     expect(await ethers.provider.getBalance(senderAddress)).to.equal(
+      parseEther("3") // 5 - 2
+    );
+  });
+
+  it("should be able to send ERC20 to another email", async function () {
+    const senderAddress = await coreContract.getAddressOfSalt(senderWalletSalt);
+
+    await erc20Contract.transfer(senderAddress, parseEther("5"));
+
+    expect(await erc20Contract.balanceOf(senderAddress)).to.equal(
+      parseEther("5")
+    );
+
+    await coreContract.connect(relayer).executeEmailOp({
+      senderPointer,
+      senderIndicator,
+      senderWalletSaltProof: {
+        walletSalt: senderWalletSalt,
+        randomNonce: 123,
+        proof: mockProof,
+      },
+      hasRecipient: true,
+      isRecipientExternal: false,
+      isRecipientInitialized: false,
+      recipientRelayer: await relayer.getAddress(),
+      recipientPointer,
+      recipientIndicator,
+      recipientExternalAddress: getAddress(
+        "0x0000000000000000000000000000000000000000"
+      ),
+      recipientWalletSaltProof: {
+        walletSalt: recipientWalletSalt,
+        randomNonce: 456,
+        proof: mockProof,
+      },
+      command: "Send",
+      emailNullifier: encodeBytes32String("NULLIFIER 2"),
+      dkimPublicKeyHash: domainPublicKeyHash,
+      domainName: "ethereum.org",
+      maskedSubjectStr: "Send 2 TST to ",
+      emailProof: mockProof,
+      amount: parseEther("2"),
+      tokenName: "TST",
+    });
+
+    expect(await erc20Contract.balanceOf(senderAddress)).to.equal(
       parseEther("3") // 5 - 2
     );
   });
