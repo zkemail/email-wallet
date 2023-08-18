461782c9f3a9cf9ef21fbd8628414692e45223a0
Yush G <aayushgupta05@gmail.com>
2023-08-18 12:20:05 -0400

    Update TokenRegistry.sol

--- packages/contracts/src/TokenRegistry.sol
+++ packages/contracts/src/TokenRegistry.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+    Registry for storing tokenName to address across chains
+    Return address(0x11...) if tokenName is 1
+
+    TODO: Store popular token address as constants would save gas
+ */
 contract TokenRegistry is Ownable {
     // Define a structure that represents a token
     struct Token {
