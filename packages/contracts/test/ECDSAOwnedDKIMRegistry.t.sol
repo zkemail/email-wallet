066b0a0ba5195dcd2dcc4178cbd03f2c4922e3eb
SoraSuegami <suegamisora@gmail.com>
2023-10-22 13:49:26 +0900

    Add dfinity-oracle case test to ECDSAOwner test.

--- packages/contracts/test/ECDSAOwnedDKIMRegistry.t.sol
+++ packages/contracts/test/ECDSAOwnedDKIMRegistry.t.sol
@@ -74,36 +74,24 @@ contract ECDSAOwnedDKIMRegistryTest is Test {
         require(registry.getDKIMPublicKeyHash(domainName) == publicKeyHash, "Invalid public key hash");
     }
 
-    // function test_RevertIfNonceInvalid() public {
-    //     vm.chainId(1);
-    //     string memory signedMsg = string.concat(
-    //         "chain_id=",
-    //         block.chainid.toString(),
-    //         ";selector=",
-    //         selector,
-    //         ";domain=",
-    //         domainName,
-    //         ";nonce=",
-    //         1.toHexString(32),
-    //         ";public_key_hash=",
-    //         uint256(publicKeyHash).toHexString(),
-    //         ";"
-    //     );
-    //     bytes32 digest = bytes(signedMsg).toEthSignedMessageHash();
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, digest);
-    //     bytes memory signature = abi.encodePacked(r, s, v);
-    //     require(registry.nonceOfDomain(domainName) == 0, "Invalid nonce");
-    //     vm.expectRevert("Invalid signature");
-    //     registry.setDKIMPublicKeyHash(
-    //         selector,
-    //         domainName,
-    //         publicKeyHash,
-    //         signature
-    //     );
-    //     require(registry.getDKIMPublicKeyHash(domainName) == bytes32(0), "Invalid public key hash");
-    //     require(registry.nonceOfDomain(domainName) == 0, "Invalid nonce");
-
-    // }
+    function test_RevertIfExpired() public {
+       vm.chainId(1);
+        uint timestamp = block.timestamp;
+        string memory signedMsg = registry.computeSignedMsg(selector, domainName, publicKeyHash, timestamp);
+        bytes32 digest = bytes(signedMsg).toEthSignedMessageHash();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        vm.warp(timestamp + signValidityDuration + 1);
+        vm.expectRevert("Signature expired");
+        registry.setDKIMPublicKeyHash(
+            selector,
+            domainName,
+            timestamp,
+            publicKeyHash,
+            signature
+        );
+        require(registry.getDKIMPublicKeyHash(domainName) == bytes32(0), "Invalid public key hash");
+    }
 
     function test_RevertIfSignatureInvalid() public {
         vm.chainId(1);
@@ -139,5 +127,23 @@ contract ECDSAOwnedDKIMRegistryTest is Test {
             signature
         );
         require(registry.getDKIMPublicKeyHash(domainName) == bytes32(0), "Invalid public key hash");
+    }
+
+    function test_Dfinity_Oracle_Response() public {
+        vm.chainId(1);
+        registry = new ECDSAOwnedDKIMRegistry(0x2F6e79a6E1a982a49CA248B70b02F76e921aF400,signValidityDuration);
+        selector = "20230601";
+        domainName = "gmail.com";
+        publicKeyHash = 0x0ea9c777dc7110e5a9e89b13f0cfc540e3845ba120b2b6dc24024d61488d4788;
+        uint timestamp = 1697948713;
+        vm.warp(timestamp + signValidityDuration);
+        registry.setDKIMPublicKeyHash(
+            selector,
+            domainName,
+            timestamp,
+            publicKeyHash,
+            vm.parseBytes("0x875fae3da3e58a97971663934b3ddafd4057706ddb7281de07d25d51e3587c3b179c4fcc45b6710bacde082933d22e69076f4b49da02273ee30a3cc5d04febe81c")
+        );
+        require(registry.getDKIMPublicKeyHash(domainName) == publicKeyHash, "Invalid public key hash");
     }
 }
