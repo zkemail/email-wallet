6593dd83b4229c576bc2b7fca54fd8c1cfb298ee
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-10-31 14:03:14 +0400

    chore(relayer): remove EML_PATH key from .env

--- packages/relayer/src/strings.rs
+++ packages/relayer/src/strings.rs
@@ -1,6 +1,5 @@
 // Config strings
 pub const ZK_EMAIL_PATH_KEY: &str = "LOCAL_ZK_EMAIL_CIRCOM_PATH";
-pub const INCOMING_EML_PATH: &str = "MODAL_INCOMING_EML_PATH";
 pub const IMAP_DOMAIN_NAME_KEY: &str = "IMAP_DOMAIN_NAME";
 pub const IMAP_PORT_KEY: &str = "IMAP_PORT";
 pub const IMAP_AUTH_TYPE_KEY: &str = "AUTH_TYPE";
