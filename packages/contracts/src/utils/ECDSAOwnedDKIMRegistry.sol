8bb3d4649d8e7b30595284d68be535838cd6116e
Saleel <saleel@saleel.xyz>
2023-11-03 12:08:14 +0300

    chore: add signature reply attach prtoection to dkimreg

--- packages/contracts/src/utils/ECDSAOwnedDKIMRegistry.sol
+++ packages/contracts/src/utils/ECDSAOwnedDKIMRegistry.sol
@@ -11,10 +11,16 @@ contract ECDSAOwnedDKIMRegistry is IDKIMRegistry {
     using Strings for *;
     using ECDSA for *;
 
-    // mapping(string => uint256) public nonceOfDomain;
-    uint signValidityDuration;
     DKIMRegistry public dkimRegistry;
+
+    // Addres of the signer who can set DKIM public key hash
     address public signer;
+
+    // Duration for which the signature is valid to be accepted for setting DKIM public key hash
+    uint signValidityDuration;
+
+    // Historical mapping of all used pub key hashes to prevent replay attacks
+    mapping(bytes32 => bool) public usedPublicKeyHashes;
 
     constructor(address _signer, uint256 _signValidityDuration) {
         dkimRegistry = new DKIMRegistry();
@@ -33,11 +39,16 @@ contract ECDSAOwnedDKIMRegistry is IDKIMRegistry {
         require(bytes(domainName).length != 0, "Invalid domain name");
         require(publicKeyHash != bytes32(0), "Invalid public key hash");
         require(block.timestamp - timestamp <= signValidityDuration, "Signature expired");
+        require(!usedPublicKeyHashes[publicKeyHash], "Public key hash already used");
+        
+        // Verify signature
         string memory signedMsg = computeSignedMsg(selector, domainName, publicKeyHash, timestamp);
         bytes32 digest = bytes(signedMsg).toEthSignedMessageHash();
         address recoveredSigner = digest.recover(signature);
         require(recoveredSigner == signer, "Invalid signature");
+        
         dkimRegistry.setDKIMPublicKeyHash(domainName, publicKeyHash);
+        usedPublicKeyHashes[publicKeyHash] = true;
     }
 
     function getDKIMPublicKeyHash(string memory domainName) public view returns (bytes32) {
