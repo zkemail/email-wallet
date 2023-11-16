387466261e6a996a0e8b74ca7ce57fd9da22d133
SoraSuegami <suegamisora@gmail.com>
2023-11-16 13:32:40 +0900

    Update dkim client.

--- packages/relayer/src/dkim_oracle.rs
+++ packages/relayer/src/dkim_oracle.rs
@@ -1,19 +1,16 @@
-use ic_utils::*;
-use ic_utils::canister::*;
+use anyhow::anyhow;
+use candid::utils::encode_args;
+use candid::{CandidType, Principal};
 use ic_agent::agent::*;
 use ic_agent::identity::*;
+use ic_utils::canister::*;
 use ic_utils::interfaces::wallet::*;
-use candid::utils::{encode_args};
-use candid::{CandidType, Principal};
+use ic_utils::*;
 use serde::{Deserialize, Serialize};
-use anyhow::{anyhow};
-
-
 
 #[derive(Debug, Clone)]
 pub struct DkimOracleClient<'a> {
-    pub(crate) canister: Canister<'a>
-
+    pub(crate) canister: Canister<'a>,
 }
 
 #[derive(Default, CandidType, Deserialize, Debug, Clone)]
@@ -27,26 +24,36 @@ pub struct SignedDkimPublicKey {
 }
 
 impl<'a> DkimOracleClient<'a> {
-    pub fn gen_agent(pem_path:&str) -> anyhow::Result<Agent> {
-        let identity = BasicIdentity::from_pem_file(pem_path)?;
+    pub fn gen_agent(pem_path: &str) -> anyhow::Result<Agent> {
+        let identity = Secp256k1Identity::from_pem_file(pem_path)?;
         let agent = AgentBuilder::default().with_identity(identity).build()?;
         Ok(agent)
     }
 
-    pub fn new(canister_id: &str, agent:&'a Agent) -> anyhow::Result<Self> {
-        let canister = CanisterBuilder::new().with_canister_id(canister_id).with_agent(&agent).build()?;
-        Ok(Self {
-            canister
-        })
+    pub fn new(canister_id: &str, agent: &'a Agent) -> anyhow::Result<Self> {
+        let canister = CanisterBuilder::new()
+            .with_canister_id(canister_id)
+            .with_agent(&agent)
+            .build()?;
+        Ok(Self { canister })
     }
 
-    pub async fn request_signature(&self, selector: &str, domain: &str) -> anyhow::Result<SignedDkimPublicKey> {
-        let request = self.canister.update("sign_dkim_public_key").with_arg(&encode_args((selector, domain))?).build::<(Result<SignedDkimPublicKey, String>,)>();
-        let response = request.call_and_wait_one::<Result<SignedDkimPublicKey, String>>().await?.map_err(|e| anyhow!(format!("Error from canister: {:?}", e)))?;
-        
+    pub async fn request_signature(
+        &self,
+        selector: &str,
+        domain: &str,
+    ) -> anyhow::Result<SignedDkimPublicKey> {
+        let request = self
+            .canister
+            .update("sign_dkim_public_key")
+            .with_arg(&encode_args((selector, domain))?)
+            .build::<(Result<SignedDkimPublicKey, String>,)>();
+        let response = request
+            .call_and_wait_one::<Result<SignedDkimPublicKey, String>>()
+            .await?
+            .map_err(|e| anyhow!(format!("Error from canister: {:?}", e)))?;
+
         // let result = Decode!(&response, String)?;
         Ok(response)
     }
-
-
 }
