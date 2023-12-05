7a00252d8ca74381c1e349dfa95e68a1ed9078fa
wshino <wshinohara@gmail.com>
2023-12-05 20:56:35 +0900

    Update deploy script for TokenRegistry.

--- packages/contracts/script/DeployTokenRegistry.s.sol
+++ packages/contracts/script/DeployTokenRegistry.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import "../src/utils/TokenRegistry.sol";
@@ -16,10 +17,10 @@ contract Deploy is Script {
         vm.startBroadcast(deployerPrivateKey);
         TokenRegistry tokenRegistryImpl = new TokenRegistry();
         bytes memory data = abi.encodeWithSelector(
-            TokenRegistry(tokenRegistryImpl).initialize.selector,
-        );            
+            TokenRegistry(tokenRegistryImpl).initialize.selector
+        );
         ERC1967Proxy proxy = new ERC1967Proxy(address(tokenRegistryImpl), data);
-        tokenRegistry = TokenRegistry(payable(address(proxy)));
+        TokenRegistry tokenRegistry = TokenRegistry(payable(address(proxy)));
 
         vm.stopBroadcast();
 
