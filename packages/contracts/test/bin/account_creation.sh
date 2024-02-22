ceeaff44e83f2d6d712973777de5439c9df3cdd8
wshino <wshinohara@gmail.com>
2024-02-22 22:08:14 +0900

    Remove account key from account_creation.sh and add account_creation_test1.eml under contracts/test/emails.

--- packages/contracts/test/bin/account_creation.sh
+++ packages/contracts/test/bin/account_creation.sh
@@ -2,7 +2,6 @@
 
 EMAIL_FILE=$1
 RELAYER_RAND=$2
-ACCOUNT_KEY=$3
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 WORKSPACE_DIR="${SCRIPT_DIR}/../../"
@@ -10,7 +9,6 @@ INPUT_FILE="${SCRIPT_DIR}/../build_integration/account_creation_input.json"
 yarn workspace @email-wallet/circom gen-account-creation-input \
     --email-file $EMAIL_FILE \
     --relayer-rand $RELAYER_RAND \
-    --account-key $ACCOUNT_KEY \
     --input-file $INPUT_FILE \
     --prove
 exit 0
