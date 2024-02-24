7e56eb18d27375c43b260d6b9fe5ffad21d9e210
wshino <wshinohara@gmail.com>
2024-02-24 23:40:29 +0900

    [WIP] Fix testIntegration_Transfer_ETH_To_Internal.

--- packages/contracts/src/handlers/UnclaimsHandler.sol
+++ packages/contracts/src/handlers/UnclaimsHandler.sol
@@ -14,7 +14,7 @@ import "../Wallet.sol";
 import "./RelayerHandler.sol";
 import "../interfaces/IVerifier.sol";
 import "./AccountHandler.sol";
-
+import "forge-std/console.sol";
 contract UnclaimsHandler is ReentrancyGuard, Initializable, UUPSUpgradeable, OwnableUpgradeable {
     using SafeERC20 for IERC20;
 
@@ -203,7 +203,7 @@ contract UnclaimsHandler is ReentrancyGuard, Initializable, UUPSUpgradeable, Own
         (string memory relayerEmailAddr,) = relayerHandler.relayers(msg.sender);
         require(bytes(relayerEmailAddr).length != 0, "caller is not a relayer");
 
-        require(verifier.verifyClaimFundProof(recipientWalletSalt, fund.emailAddrCommit, proof), "invalid proof");
+        require(verifier.verifyClaimFundProof(fund.emailAddrCommit, recipientWalletSalt, proof), "invalid proof");
 
         address recipientAddr = accountHandler.getWalletOfSalt(recipientWalletSalt);
 
