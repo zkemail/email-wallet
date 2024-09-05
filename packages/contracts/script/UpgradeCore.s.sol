c10b443129d3b0b6047d2fc88ea12681c5ed8693
SoraSuegami <suegamisora@gmail.com>
2024-09-05 17:32:41 +0900

    Add upgrade contract for core

--- packages/contracts/script/UpgradeCore.s.sol
+++ packages/contracts/script/UpgradeCore.s.sol
@@ -4,20 +4,19 @@ pragma solidity ^0.8.13;
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
-import "../src/utils/TokenRegistry.sol";
+import "../src/EmailWalletCore.sol";
 
-contract Deploy is Script {
+contract Upgrade is Script {
     function run() external {
-        
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         if (deployerPrivateKey == 0) {
             console.log("PRIVATE_KEY env var not set");
             return;
         }
 
-        address tokenRegistry = vm.envAddress("TOKEN_REGISTRY");
-        if (tokenRegistry == address(0)) {
-            console.log("TOKEN_REGISTRY env var not set. Deploy TokenRegistry and set env var");
+        address core = vm.envAddress("CORE");
+        if (core == address(0)) {
+            console.log("CORE env var not set. Deploy EmailWalletCore and set env var");
             return;
         }
 
@@ -25,19 +24,19 @@ contract Deploy is Script {
 
         // FIXME Set new TokenRegistry implementation
         // e.g. TokenRegistryV2 tokenRegistryImpl = new TokenRegistryV2();
-        TokenRegistry tokenRegistryImpl = new TokenRegistry();
+        EmailWalletCore coreImpl = new EmailWalletCore();
 
-        TokenRegistry tokenRegistryProxy = TokenRegistry(payable(address(tokenRegistry)));
-        tokenRegistryProxy.upgradeTo(address(tokenRegistryImpl));
+        EmailWalletCore coreProxy = EmailWalletCore(payable(core));
+        coreProxy.upgradeTo(address(coreImpl));
 
         // If you want to call some v2 function, refer to the following steps
         //
         // TokenRegistryV2 tokenRegistryV2 = TokenRegistryV2(address(tokenRegistry));
         // address usdc = tokenRegistry.getTokenAddress(0, "USDC");
-                
+
         vm.stopBroadcast();
 
-        console.log("TokenRegistry implementation deployed at: %s", address(tokenRegistryImpl));
+        console.log("Core implementation deployed at: %s", address(coreImpl));
         console.log("---- DONE ----");
     }
 }
